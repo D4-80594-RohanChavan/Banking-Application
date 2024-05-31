d72d5175b6e0ee2eb41a271cca7991a52fc77c4b
Rohan Chavan <97110306+D4-80594-RohanChavan@users.noreply.github.com>
2024-05-31 14:48:14 +0530

    Add files via upload
    basic temp for project

---
+++ krasvBank/src/test/java/com/axis/team2/technoTribe/krasvBank/KrasvBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axis.team2.technoTribe.krasvBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrasvBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

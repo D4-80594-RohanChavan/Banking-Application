7fd49462ffdf28f8ca7da205aba7098cf108b85c
kaushalkumar98 <134416835+kaushalkumar98@users.noreply.github.com>
2024-06-03 09:53:20 +0530

    Account, Fund Transfer & Transaction

---
+++ krasvbank/src/main/java/com/axis/team2/technotribe/krasvbank/BrasvbankApplication.java
@@ -0,0 +1,13 @@
+package com.axis.team2.technotribe.krasvbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasvbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasvbankApplication.class, args);
+	}
+
+}
---
+++ krasvbank/src/test/java/com/axis/team1/technotribe/krasvbank/BrasvbankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axis.team1.technotribe.krasvbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasvbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

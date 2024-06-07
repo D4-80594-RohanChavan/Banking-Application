4e37cb939292051af8677460c617f673e01e964b
Rohan Chavan <97110306+D4-80594-RohanChavan@users.noreply.github.com>
2024-06-08 03:14:47 +0530

    Add files via upload

--- KrasvBank_Backend/spring_boot_backend_security_jwt_template/spring_boot_backend_security_jwt_template/src/main/java/com/app/team2/technotribe/krasvbank/bankStatement/BankStatement.java
+++ KrasvBank_Backend/spring_boot_backend_security_jwt_template/spring_boot_backend_security_jwt_template/src/main/java/com/app/team2/technotribe/krasvbank/bankStatement/BankStatement.java
@@ -42,104 +42,102 @@ public class BankStatement {
 	@Autowired
 	EmailService emailService;
 	
-
-	
-	
 	public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate) {
-		LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
-		LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
-		List<Transaction> transactionList = transactionRepository.findAll().stream()
-				.filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-				.filter(transaction -> transaction.getCreatedAt().isEqual(start))
-				.filter(transaction -> transaction.getCreatedAt().isEqual(end)).toList();
+	    LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
+	    LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
+	    List<Transaction> transactionList = transactionRepository.findAll().stream()
+	            .filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
+	            .filter(transaction -> !transaction.getCreatedAt().isBefore(start) && !transaction.getCreatedAt().isAfter(end))
+	            .toList();
 
-		User user = userRepository.findByAccountNumber(accountNumber);
-		String customerName = user.getName();
 
-		Rectangle statementSize = new Rectangle(PageSize.A4);
-		Document document = new Document(statementSize);
-		log.info("Setting size of document");
-		try {
-			OutputStream outputStream = new FileOutputStream(FILE);
-			PdfWriter.getInstance(document, outputStream);
-			document.open();
-
-			PdfPTable bankInfoTable = new PdfPTable(1);
-			PdfPCell bankName = new PdfPCell(new Phrase("Krasv Bank"));
-			bankName.setBorder(0);
-			bankName.setBackgroundColor(BaseColor.RED);
-			bankName.setPadding(20f);
-
-			PdfPCell bankAddress = new PdfPCell(new Phrase("72, Maharashtra"));
-			bankAddress.setBorder(0);
-			bankInfoTable.addCell(bankName);
-			bankInfoTable.addCell(bankAddress);
-
-			PdfPTable statementInfo = new PdfPTable(2);
-			PdfPCell customerInfo = new PdfPCell(new Phrase("Start Date: " + startDate));
-			customerInfo.setBorder(0);
-			PdfPCell statement = new PdfPCell(new Phrase("STATEMENT OF ACCOUNT"));
-			statement.setBorder(0);
-			PdfPCell stopDate = new PdfPCell(new Phrase("End Date" + endDate));
-			stopDate.setBorder(0);
-			PdfPCell name = new PdfPCell(new Phrase("Customer Name: " + customerName));
-			name.setBorder(0);
-			PdfPCell space = new PdfPCell();
-			space.setBorder(0);
-			PdfPCell address = new PdfPCell(new Phrase("Customer Address " + user.getAddress()));
-			space.setBorder(0);
-
-			PdfPTable transactionsTable = new PdfPTable(4);
-			PdfPCell date = new PdfPCell(new Phrase("DATE"));
-			date.setBackgroundColor(BaseColor.RED);
-			date.setBorder(0);
-			PdfPCell transactionType = new PdfPCell(new Phrase("TRANSACTION TYPE"));
-			transactionType.setBackgroundColor(BaseColor.RED);
-			transactionType.setBorder(0);
-			PdfPCell transactionAmount = new PdfPCell(new Phrase("TRANSACTION AMOUNT"));
-			transactionAmount.setBackgroundColor(BaseColor.RED);
-			transactionAmount.setBorder(0);
-			PdfPCell status = new PdfPCell(new Phrase("STATUS"));
-			status.setBackgroundColor(BaseColor.RED);
-			status.setBorder(0);
-
-			transactionsTable.addCell(date);
-			transactionsTable.addCell(transactionType);
-			transactionsTable.addCell(transactionAmount);
-			transactionsTable.addCell(status);
-
-			transactionList.forEach(transaction -> {
-				transactionsTable.addCell(new Phrase(transaction.getCreatedAt().toString()));
-				transactionsTable.addCell(new Phrase(transaction.getTransactionType()));
-				transactionsTable.addCell(new Phrase(transaction.getAmount().toString()));
-				transactionsTable.addCell(new Phrase(transaction.getStatus()));
-			});
-
-			statementInfo.addCell(customerInfo);
-			statementInfo.addCell(statement);
-			statementInfo.addCell(endDate);
-			statementInfo.addCell(name);
-			statementInfo.addCell(space);
-			statementInfo.addCell(address);
-
-			document.add(bankInfoTable);
-			document.add(statementInfo);
-			document.add(transactionsTable);
-
-			document.close();
-			
-			EmailDetails emailDetails=EmailDetails.builder()
-					.recipient(user.getEmail())
-					.subject("ACCOUNT STATEMENT")
-					.messageBody("Kindly find your requested account statement attached!")
-					.attachment(FILE)
-					.build();
-			
-			emailService.sendEmailWithAttachment(emailDetails);
-
-		} catch (FileNotFoundException | DocumentException e) {
-			log.log(Level.SEVERE, "Error creating PDF", e);
-		}
+//		User user = userRepository.findByAccountNumber(accountNumber);
+//		String customerName = user.getName();
+//
+//		Rectangle statementSize = new Rectangle(PageSize.A4);
+//		Document document = new Document(statementSize);
+//		log.info("Setting size of document");
+//		try {
+//			OutputStream outputStream = new FileOutputStream(FILE);
+//			PdfWriter.getInstance(document, outputStream);
+//			document.open();
+//
+//			PdfPTable bankInfoTable = new PdfPTable(1);
+//			PdfPCell bankName = new PdfPCell(new Phrase("Krasv Bank"));
+//			bankName.setBorder(0);
+//			bankName.setBackgroundColor(BaseColor.RED);
+//			bankName.setPadding(20f);
+//
+//			PdfPCell bankAddress = new PdfPCell(new Phrase("72, Maharashtra"));
+//			bankAddress.setBorder(0);
+//			bankInfoTable.addCell(bankName);
+//			bankInfoTable.addCell(bankAddress);
+//
+//			PdfPTable statementInfo = new PdfPTable(2);
+//			PdfPCell customerInfo = new PdfPCell(new Phrase("Start Date: " + startDate));
+//			customerInfo.setBorder(0);
+//			PdfPCell statement = new PdfPCell(new Phrase("STATEMENT OF ACCOUNT"));
+//			statement.setBorder(0);
+//			PdfPCell stopDate = new PdfPCell(new Phrase("End Date" + endDate));
+//			stopDate.setBorder(0);
+//			PdfPCell name = new PdfPCell(new Phrase("Customer Name: " + customerName));
+//			name.setBorder(0);
+//			PdfPCell space = new PdfPCell();
+//			space.setBorder(0);
+//			PdfPCell address = new PdfPCell(new Phrase("Customer Address " + user.getAddress()));
+//			space.setBorder(0);
+//
+//			PdfPTable transactionsTable = new PdfPTable(4);
+//			PdfPCell date = new PdfPCell(new Phrase("DATE"));
+//			date.setBackgroundColor(BaseColor.RED);
+//			date.setBorder(0);
+//			PdfPCell transactionType = new PdfPCell(new Phrase("TRANSACTION TYPE"));
+//			transactionType.setBackgroundColor(BaseColor.RED);
+//			transactionType.setBorder(0);
+//			PdfPCell transactionAmount = new PdfPCell(new Phrase("TRANSACTION AMOUNT"));
+//			transactionAmount.setBackgroundColor(BaseColor.RED);
+//			transactionAmount.setBorder(0);
+//			PdfPCell status = new PdfPCell(new Phrase("STATUS"));
+//			status.setBackgroundColor(BaseColor.RED);
+//			status.setBorder(0);
+//
+//			transactionsTable.addCell(date);
+//			transactionsTable.addCell(transactionType);
+//			transactionsTable.addCell(transactionAmount);
+//			transactionsTable.addCell(status);
+//
+//			transactionList.forEach(transaction -> {
+//				transactionsTable.addCell(new Phrase(transaction.getCreatedAt().toString()));
+//				transactionsTable.addCell(new Phrase(transaction.getTransactionType()));
+//				transactionsTable.addCell(new Phrase(transaction.getAmount().toString()));
+//				transactionsTable.addCell(new Phrase(transaction.getStatus()));
+//			});
+//
+//			statementInfo.addCell(customerInfo);
+//			statementInfo.addCell(statement);
+//			statementInfo.addCell(endDate);
+//			statementInfo.addCell(name);
+//			statementInfo.addCell(space);
+//			statementInfo.addCell(address);
+//
+//			document.add(bankInfoTable);
+//			document.add(statementInfo);
+//			document.add(transactionsTable);
+//
+//			document.close();
+//			
+//			EmailDetails emailDetails=EmailDetails.builder()
+//					.recipient(user.getEmail())
+//					.subject("ACCOUNT STATEMENT")
+//					.messageBody("Kindly find your requested account statement attached!")
+//					.attachment(FILE)
+//					.build();
+//			
+//			emailService.sendEmailWithAttachment(emailDetails);
+//
+//		} catch (FileNotFoundException | DocumentException e) {
+//			log.log(Level.SEVERE, "Error creating PDF", e);
+//		}
 
 		return transactionList;
 	}
--- KrasvBank_Backend/spring_boot_backend_security_jwt_template/spring_boot_backend_security_jwt_template/src/main/java/com/app/team2/technotribe/krasvbank/controller/AdminController.java
+++ KrasvBank_Backend/spring_boot_backend_security_jwt_template/spring_boot_backend_security_jwt_template/src/main/java/com/app/team2/technotribe/krasvbank/controller/AdminController.java
@@ -1,9 +1,15 @@
 package com.app.team2.technotribe.krasvbank.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
 public class AdminController {
 
-	public AdminController() {
-		// TODO Auto-generated constructor stub
+	@GetMapping
+	public String admin() {
+		return "admin page";
 	}
-
 }
